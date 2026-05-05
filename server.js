b91733cdd1a393c045a2d69874f1aeea067c4296
eusebiongoy <banzaeusebio2@gmail.com>
2026-05-05 22:02:47 +0200

    review all the projects

--- server.js
+++ server.js
@@ -2,30 +2,23 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-// Define the the application environment
+// Environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
-
-// Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
+// Required for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
 /**
-  * Configure Express middleware
-  */
-
-// Serve static files from the public directory
+ * Middleware
+ */
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set EJS as the templating engine
 app.set('view engine', 'ejs');
-
-// Tell Express where to find your templates
 app.set('views', path.join(__dirname, 'src/views'));
-
 
 /**
  * Routes
@@ -45,7 +38,15 @@ app.get('/projects', (req, res) => {
     res.render('projects', { title });
 });
 
+app.get('/categories', (req, res) => {
+    const title = 'Categories';
+    res.render('categories', { title });
+});
+
+/**
+ * Start server
+ */
 app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
 });
